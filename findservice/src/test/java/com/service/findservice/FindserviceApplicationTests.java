62d43189c063bc193d962641c52e0a01407d9084
gudongFox <1129913541@qq.com>
2021-03-15 11:53:09 +0800

    后端代码

---
+++ findservice/src/test/java/com/service/findservice/FindserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.findservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
