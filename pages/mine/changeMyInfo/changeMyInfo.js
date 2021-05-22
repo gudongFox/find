767ee6f06b384f9fa2d1e653f8cb267852930a59
liudong <1129913541@qq.com>
2021-05-22 16:46:45 +0800

    新增更新客户信息

---
+++ pages/mine/changeMyInfo/changeMyInfo.js
@@ -0,0 +1,83 @@
+// pages/mine/changeMyInfo/changeMyInfo.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    clientInfo:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this
+    var clientInfo = JSON.parse(decodeURIComponent(options.URINewInfo));
+    that.setData({
+      clientInfo:clientInfo
+    })
+  },
+  saveInfo:function(){
+    var that = this
+    var clientInfo = that.data.clientInfo
+    if(clientInfo.clientGender == "男"){
+      clientInfo.clientGender = 1
+    }else{
+      clientInfo.clientGender = 2
+    }
+    // wx.request({
+    //   url: 'http://129.211.68.243:8080/client/info',
+    //   method: 'PUT'
+    // })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
